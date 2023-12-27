1b48e98076ae10334a10e9cdb536555a8e65afc8
lysabrina <lysabrinak@gmail.com>
2023-12-26 22:03:08 -0800

    Update js to open only one question at a time

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const questions = document.getElementsByClassName("question");
+
+//loop through each question and add an event listener with the openQuestion(i)
+// O(n^2) :(
 for (let i = 0; i < questions.length; i++) {
   questions[i].addEventListener("click", () => {
-    questions[i].classList.toggle("question-open");
-    const arrow = questions[i].querySelector("img");
-    arrow.classList.toggle("flip");
-
-    const answer = questions[i].nextElementSibling;
-    answer.classList.toggle("open");
+    openQuestion(i);
   });
+}
+
+//loop through the question again and turn the previous opened question off
+//With the position argument, open the question by adding the necessary classes
+function openQuestion(position) {
+  for (let i = 0; i < questions.length; i++) {
+    const arrow = questions[i].querySelector("img");
+    const answer = questions[i].nextElementSibling;
+    if (answer.classList.contains("open")) {
+      answer.classList.toggle("open");
+      questions[i].classList.toggle("question-open");
+      arrow.classList.toggle("flip");
+    } else if (i == position) {
+      questions[i].classList.toggle("question-open");
+      arrow.classList.toggle("flip");
+      answer.classList.toggle("open");
+    }
+  }
 }
